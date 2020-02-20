9312078031bc39d93922daa12cd2e655034e2731
unknown <sanketmohapatra1999@gmail.com>
2020-02-20 21:16:37 +0530

    try

---
+++ multithread/PingpongHasA.java
@@ -0,0 +1,54 @@
+package com.multithread;
+
+
+public class PingpongHasA implements Runnable{
+
+	Thread t1,t2;
+	public PingpongHasA()
+	{
+		t1= new Thread(this);
+		t2= new Thread(this);
+		t1.setName("ping");
+		t2.setName("pong");
+		t1.start();
+		t2.start();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new PingpongHasA();
+	}
+	
+	@Override
+	public void run() {
+	Thread currentThread = Thread.currentThread();
+	if(currentThread.getName().equals("ping"))
+		{
+			for (int i =0  ; i < 20 ; i++)
+			{
+				System.out.println("ping");
+				try {
+					Thread.sleep(500);
+					} catch(InterruptedException e)
+				{
+				e.printStackTrace();
+				}
+			}
+		}
+	else if(currentThread.getName().equals("pong"))
+	{
+		for (int i =0  ; i < 20 ; i++)
+		{
+			System.out.println("\t pong");
+			try {
+				Thread.sleep(500);
+				} catch(InterruptedException e)
+			{
+			e.printStackTrace();
+			}
+		}
+	}
+	
+	}
+}	
+			
+		
---
+++ multithread/PingpongIsA.java
@@ -0,0 +1,54 @@
+package com.multithread;
+
+
+public class PingpongIsA extends Thread{
+	
+	public PingpongIsA(String threadName)
+	{
+		super.setName(threadName);
+		super.start();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		new PingpongIsA("ping");
+		new PingpongIsA("pong");
+		}
+		
+	
+	
+	@Override
+	public void run() {
+	Thread currentThread = Thread.currentThread();
+	if(currentThread.getName().equals("ping"))
+		{
+			for (int i =0  ; i < 20 ; i++)
+			{
+				System.out.println("ping");
+				try {
+					Thread.sleep(500);
+					} catch(InterruptedException e)
+				{
+				e.printStackTrace();
+				}
+			}
+		}
+	else if(currentThread.getName().equals("pong"))
+	{
+		for (int i =0  ; i < 20 ; i++)
+		{
+			System.out.println("\t pong");
+			try {
+				Thread.sleep(500);
+				} catch(InterruptedException e)
+			{
+			e.printStackTrace();
+			}
+		}
+	}
+	
+	}
+}	
+			
+		
